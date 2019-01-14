c88305d2a873189642fa5550f937c1a0da0c85db
Simon Ransome <github@nosher.net>
2019-01-14 23:11:38 +0000

    WEB-5: improve search and add form

--- index_site.py
+++ index_site.py
@@ -1,8 +1,9 @@
 import os
 import re
 from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.fields import Schema, TEXT, ID, DATETIME
 from archives.constants import *
+from datetime import *
 import sys
  
 def createIndex(root):   
@@ -11,7 +12,7 @@ def createIndex(root):
     Schema definition: title(name of file), path(as ID), content(indexed
     but not stored),textdata (stored text content)
     '''
-    schema = Schema(path = ID(stored = True), image = TEXT(stored = True), content = TEXT)
+    schema = Schema(path = ID(stored = True), image = TEXT(stored = True), content = TEXT, date = DATETIME(sortable = True))
     if not os.path.exists("index"):
         os.mkdir("index")
     ix = create_in("index", schema)
@@ -41,8 +42,13 @@ def createIndex(root):
                 if f == "details.txt":
                     full = os.path.join(path, f)
                     print (full)
+                    try:
+                        timestamp = datetime.strptime(path.split("/")[-1][0:10], "%Y-%m-%d")
+                    except ValueError:
+                        timestamp = datetime.strptime("1989-10-01", "%Y-%m-%d")
                     with open(full, "r") as fh:
                         try:
+                            first = 0 
                             text = fh.readlines()
                             if len(text) > 3: 
                                 for i in range(0, len(text)):
@@ -51,13 +57,14 @@ def createIndex(root):
                                         if len(parts) > 1:
                                             webpath = path.replace("/home/httpd/nosher.net/docs/", "")
                                             if parts[0] == "title" or parts[0] == "intro":
-                                                img = "{}/{}/{}-s.jpg".format(WEBROOT, webpath, text[3])
-                                                writer.add_document(path = webpath, content = parts[1], image = img)
+                                                img = "{}/{}/{}-s.jpg".format(WEBROOT, webpath, text[3].split("\t")[0])
+                                                writer.add_document(path = webpath, content = parts[1], image = img, date = timestamp)
                                             elif parts[0] == "locn":
                                                 pass
                                             else:
+                                                if first == 0: first = i
                                                 img = "{}/{}/{}-s.jpg".format(WEBROOT, webpath, parts[0])
-                                                writer.add_document(path = "{}/{}".format(webpath, i), content = parts[1], image = img)
+                                                writer.add_document(path = "{}/{}".format(webpath, i - first), content = parts[1], image = img, date = timestamp)
                                     except ValueError:
                                         print (path, l)
                         except UnicodeDecodeError:
--- search/views.py
+++ search/views.py
@@ -22,6 +22,7 @@ from whoosh.index import open_dir
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.qparser import QueryParser
 
+
 CONTENT = "/content/"
 
 
@@ -32,11 +33,24 @@ def search(request):
     qp = QueryParser("content", schema = ix.schema)
     q = qp.parse(query)
     results =[] 
+    start = 0
+    page = 50
     with ix.searcher() as s:
-        results = s.search(q, limit = 1000)
+        results = s.search(q, limit = 500, sortedby="date", reverse=True)
+        existing = []
+        filtered = []
+        for res in results:
+            if not res["path"] in existing:
+                filtered.append(res)
+                existing.append(res["path"])
+        if len(filtered) < start + page:
+            end = len(filtered) - start
+        else:
+            end = start + page
 
         context = {
-            'results': results,
+            'query': query,
+            'results': filtered[start:end],
             'server': WEBROOT,
         }
         return render(request, 'search/search.html', context)
