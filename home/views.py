96fbf4d47be948fd244b8ec74e54886319c80428
Simon Ransome <siransom@microsoft.com>
2022-05-04 17:29:14 +0100

    Add photos to home page naviation

--- home/views.py
+++ home/views.py
@@ -14,8 +14,23 @@ def get_now(null):
     return date.today().year
 
 def index(request):
-
+    
+    now = datetime.now()
+    clip = now.strftime("-%m-")
+    this_month = PhotoAlbum.objects.filter(path__contains=clip)[:30]
     latest_albums = PhotoAlbum.objects.order_by('-date_created')[:6]
+    this_day = []
+    for latest in this_month:
+        path = latest.path
+        try:
+            with open("/home/httpd/nosher.net/docs/images/{}/details.txt".format(path)) as fh:
+                lines = fh.readlines()
+                bits = lines[3].split("\t")
+                if (len(bits) > 1):
+                    this_day.append(("{}/{}-s.jpg".format(path, bits[0]), path))
+        except Exception:
+            pass
+            
     #for album in latest_albums:
     #    path = album.path
     #    with open("/home/httpd/nosher.net/docs/images/{}")
@@ -24,6 +39,7 @@ def index(request):
     pic_url = "https://static.nosher.net/graphics/v3/splash{}.jpg".format(splash[random.randint(0, len(splash) - 1)])
     context = {
         'WEBROOT': WEBROOT,
+        'this_month': this_day[0:24],
         'latest_albums': latest_albums,
         'random_photo': pic_url,
         'staticServer': WEBROOT,
