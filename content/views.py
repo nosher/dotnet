dc2357e9beca138c291d1e39a20363f0689dca23
Simon Ransome <github@nosher.net>
2023-01-17 23:56:50 +0000

    WEB-30: add a recipe archive and parsing code

--- content/views.py
+++ content/views.py
@@ -62,7 +62,38 @@ def _get_page(section, page):
     path = "/home/httpd/nosher.net/docs/content/{}/{}".format(section, page)
     mtime = datetime.date.fromtimestamp(os.stat(path)[7])
     with open(path, encoding="utf-8") as fh:
-        body = fh.read()
+        body = _format_page(fh.read())
         t = Template(body)
         return (t.render(Context({'staticServer': WEBROOT})), mtime)
         
+def _format_page(page):
+    # resolve any custom tags in content, e.g. for recipes
+    if page.find("[recipe]") > -1:
+        (start, rest) = page.split("[recipe]")
+        (recipe, rest) = rest.split("[end]")
+        parts = recipe.split("--") # intro, recipe, method, serve
+        if len(parts) > 3:
+            imgmatch = '\|(.*?)\|'
+            method = "<ol>\n"
+            exc_empty = lambda item: item != ""
+            recipe = parts[1].split("\n")
+            filtered_recipe = filter(exc_empty, recipe)
+            steps = parts[2].split("\n")
+            filtered_steps = filter(exc_empty, steps)
+            for step in filtered_steps:
+                imgs = re.search(imgmatch, step)
+                if imgs is not None:
+                    img = imgs.group().replace("|", "")
+                    tag = """<br /><img class="recimg" src="{}/content/recipes/images/{}.jpg" /><br />""".format(WEBROOT, img)
+                    step = step.replace(imgs.group(), tag)
+                method = method + "<li>{}</li>\n".format(step) 
+            method = method + "</ol>"
+            extras = ""
+            if len(parts) > 4 and parts[4].strip() != "":
+                extras = "\n<diet->Healthier variation: {}".format(parts[4])
+            return """<p><a href="/content/recipes/" class="dots">Recipe index</a></p>{}\n<intro->{}</intro->\n<recipe->{}</recipe->\n<method->\n{}\n</method->\n<serve->{}</serve->\n{}\n{}"""\
+                .format(start, parts[0], "\n".join(filtered_recipe), method, parts[3], rest, extras)
+        else:
+            return page
+    else:
+        return page
